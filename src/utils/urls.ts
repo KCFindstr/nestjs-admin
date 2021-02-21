9680fc82e443e775965ab51845a0fb84ef647550
KCFindstr <792491320@qq.com>
2021-02-20 17:10:00 -0800

    Modify URLs

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -4,42 +4,42 @@ import { parseName } from './formatting';
 import { getPrimaryKeyValue } from './entity';
 
 export function indexUrl() {
-  return `/admin`;
+  return `/api/admin`;
 }
 
 export function loginUrl() {
-  return `/admin/login`;
+  return `/api/admin/login`;
 }
 
 export function logoutUrl() {
-  return `/admin/logout`;
+  return `/api/admin/logout`;
 }
 
 export function changeListUrl(section: AdminSection, metadata: EntityMetadata) {
-  return `/admin/${parseName(section.name)}/${parseName(metadata.name)}`;
+  return `/api/admin/${parseName(section.name)}/${parseName(metadata.name)}`;
 }
 
 export function listActionUrl(section: AdminSection, metadata: EntityMetadata) {
-  return `/admin/${parseName(section.name)}/${parseName(metadata.name)}/action`;
+  return `/api/admin/${parseName(section.name)}/${parseName(metadata.name)}/action`;
 }
 
 export function changeUrl(section: AdminSection, metadata: EntityMetadata, entity: object) {
   const primaryKey = urlEncodePrimaryKey(getPrimaryKeyValue(metadata, entity));
-  return `/admin/${parseName(section.name)}/${parseName(metadata.name)}/${primaryKey}/change`;
+  return `/api/admin/${parseName(section.name)}/${parseName(metadata.name)}/${primaryKey}/change`;
 }
 
 export function changeActionUrl(section: AdminSection, metadata: EntityMetadata, entity: object) {
   const primaryKey = urlEncodePrimaryKey(getPrimaryKeyValue(metadata, entity));
-  return `/admin/${parseName(section.name)}/${parseName(metadata.name)}/${primaryKey}/action`;
+  return `/api/admin/${parseName(section.name)}/${parseName(metadata.name)}/${primaryKey}/action`;
 }
 
 export function deleteUrl(section: AdminSection, metadata: EntityMetadata, entity: object) {
   const primaryKey = urlEncodePrimaryKey(getPrimaryKeyValue(metadata, entity));
-  return `/admin/${parseName(section.name)}/${parseName(metadata.name)}/${primaryKey}/delete`;
+  return `/api/admin/${parseName(section.name)}/${parseName(metadata.name)}/${primaryKey}/delete`;
 }
 
 export function addUrl(section: AdminSection, metadata: EntityMetadata) {
-  return `/admin/${parseName(section.name)}/${parseName(metadata.name)}/add`;
+  return `/api/admin/${parseName(section.name)}/${parseName(metadata.name)}/add`;
 }
 
 function urlEncodePrimaryKey(primaryKey: string | number | object) {
